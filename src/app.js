fa4911178dd9e280f6a9fdea56ca298084a5b28b
anup thakur <anksthakur7@gmail.com>
2025-02-03 15:53:12 +0530

    api pending controller done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./user/user.routes.js"
 
 const app = express()
 // sirf kon kon se path ko access kar ske
@@ -18,5 +19,11 @@ app.use(express.urlencoded())
 app.use(express.static("public"))
 // user ke browser se cookies access kar ske 
 app.use(cookieParser());
+
+//routes
+//app.get hum tab krte hai jab humne jab route and controller sath m likhte hai
+// jab hum alg alg define krte hai to middleware use krna pdta hai 
+app.use("/api/v1/users", userRouter)
+//http://localhost:5001/api/v1/users/register
 
 export {app}
